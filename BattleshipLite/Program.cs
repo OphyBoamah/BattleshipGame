a0c6b93c06d8a0d3136abc4f8c3ffab82c03fb02
Ophy Boamah <ophybee8@gmail.com>
2021-02-20 22:15:27 +0000

    Second UI section

--- BattleshipLite/Program.cs
+++ BattleshipLite/Program.cs
@@ -11,11 +11,63 @@ namespace BattleshipLite
         public static void Main(string[] args)
         {
             WelcomeMessage();
-            PlayerInfoModel player1 = CreatePlayer("Player 1");
-            PlayerInfoModel player2 = CreatePlayer("Player 2");
+            PlayerInfoModel activePlayer = CreatePlayer("Player 1");
+            PlayerInfoModel opponent = CreatePlayer("Player 2");
+            PlayerInfoModel winner = null;
+
+            do
+            {
+                //Display grid of activePlayer on where they fired
+                DisplayShotGrid(activePlayer);
+                //Ask activePlayer for point
+                //Determine if it is a valid shot
+                //Determine shot results
+                RecordPlayerShot(activePlayer);
+                //Determine if the game is over
+                //If game is over, set activePlayer as winner
+                //else, swap positions (activePlayer to opponent)
+
+            } while (winner == null);
+            
             Console.ReadLine();
         }
 
+        private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+        {
+            //Asks for a shot (we ask for "B2")
+            //Determine what row and column that is
+            //Determine if that is not a valid 
+            //Go back to the beginning if not a valid shot
+        }
+
+        private static void DisplayShotGrid(PlayerInfoModel activePlayer)
+        {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+            foreach (var gridSpot in activePlayer.ShotGrid)
+            {
+                if (gridSpot.SpotLetter != currentRow)
+                {
+                    Console.WriteLine();
+                    currentRow = gridSpot.SpotLetter;
+                }
+                if (gridSpot.Status == GridSpotStatus.Empty)
+                {
+                    Console.WriteLine($"{gridSpot.SpotLetter}{gridSpot.SpotNumber}");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Hit)
+                {
+                    Console.WriteLine("X");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Miss)
+                {
+                    Console.WriteLine("O");
+                }
+                else
+                {
+                    Console.WriteLine("?");
+                }
+            }
+        }
         private static void WelcomeMessage()
         {
             Console.WriteLine("Hello Everyone. Welcome to Battleship Lite Game");
